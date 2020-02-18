8613441637e4a5336dc47d5630aed7ec5aefc802
dinesh <dineshreddy3600@.com>
2020-02-18 13:23:52 +0530

    first commit

---
+++ Dinesh.java
@@ -0,0 +1,8 @@
+public class Hello {
+
+    public static void main(String[] args) {
+       System.out.println("Hello, bro");
+        System.out.println("Triggering one new job from Pune");
+    }
+
+}
