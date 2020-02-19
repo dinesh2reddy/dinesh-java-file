fa614fb377560fe7c4132b02ee0cb3078eff80f5
dinesh2reddy <61173932+dinesh2reddy@users.noreply.github.com>
2020-02-19 14:03:53 +0530

    Delete Hello.java

--- Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-
-    public static void main(String[] args) {
-       System.out.println("Hello, bro");
-        System.out.println("Triggering one new job from Pune");
-    }
-
-}
